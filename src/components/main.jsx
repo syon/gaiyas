e7b829e5439b7c50b8bfe9bd3b10e7b53dbd74c3
syon <syon.xiii@gmail.com>
2017-01-29 00:28:46 +0900

    :sparkles: open and close

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,15 +1,14 @@
 /* @type */
 import React from 'react'
+import $ from 'jquery'
 const T = React.PropTypes
 
 module.exports = React.createClass({
   propTypes: {
     store: T.object.isRequired
   },
-  getInitialState() {
-    return {
-      text: 'Hello world!'
-    }
+  toggleClose() {
+    $('#chrome-extension-gaiyas').toggleClass('closed')
   },
   render() {
     const h = this.props.store.hatena
@@ -31,6 +30,7 @@ module.exports = React.createClass({
     return (
       <div className="ceg__wrap">
         <div className="ceg__header">
+          <button className="ceg__toggle" onClick={this.toggleClose}>B!</button>
           <div className="ceg__title">Hatena Bookmark</div>
           <div className="ceg__cnt">{h.count}</div>
         </div>
