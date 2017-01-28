53085bae81d9384f685ed32eb138296c796a3905
syon <syon.xiii@gmail.com>
2017-01-28 23:31:23 +0900

    :wrench: Production Build

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,11 @@ module.exports = {
   },
 
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
     // new webpack.optimize.UglifyJsPlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.DedupePlugin(),
