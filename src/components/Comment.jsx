0989b379c5085bc333c4b509aadc3b8fdf341136
syon <syon.xiii@gmail.com>
2017-02-02 21:33:31 +0900

    LazyLoad Avatar Image

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,11 +1,20 @@
 /* @type */
 import React from 'react'
+import ReactDOM from 'react-dom'
 const T = React.PropTypes
 
 module.exports = React.createClass({
   propTypes: {
     bookmark: T.object.isRequired,
-    store: T.object.isRequired
+    store: T.object.isRequired,
+    pos: T.number.isRequired
+  },
+  getInitialState() {
+    return { p: 9999, overred: false }
+  },
+  componentDidMount() {
+    const tc = ReactDOM.findDOMNode(this.refs.TheComment)
+    this.setState({ p: tc.offsetTop })
   },
   render() {
     const b = this.props.bookmark
@@ -15,10 +24,17 @@ module.exports = React.createClass({
     if (bucomeHash[b.user]) {
       s = <span className="ceg__star">{bucomeHash[b.user]}</span>
     }
+    if (this.state.p < this.props.pos + 1000 && !this.state.overred) {
+      this.setState({ overred: true })
+    }
+    let imgurl = ''
+    if (this.state.overred) {
+      imgurl = `//cdn1.www.st-hatena.com/users/${u}/profile_l.gif`
+    }
     return (
-      <div key={b.user} className="ceg__line">
+      <div key={b.user} className="ceg__line" ref="TheComment">
         <div className="ceg__avatar">
-          <img src={`//cdn1.www.st-hatena.com/users/${u}/profile_l.gif`}/>
+          <img className="ceg__avatarimg" src={imgurl}/>
           { s }
         </div>
         <div className="ceg__comment">
--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -1,5 +1,6 @@
 /* @type */
 import React from 'react'
+import ReactDOM from 'react-dom'
 import $ from 'jquery'
 import Comment from './Comment.jsx'
 const T = React.PropTypes
@@ -7,6 +8,17 @@ const T = React.PropTypes
 module.exports = React.createClass({
   propTypes: {
     store: T.object.isRequired
+  },
+  getInitialState() {
+    return { pos: 0 }
+  },
+  componentDidMount() {
+    const tcl = ReactDOM.findDOMNode(this.refs.TheCommentList)
+    if (tcl) {
+      tcl.onscroll = (ev) => {
+        this.setState({ pos: ev.target.scrollTop })
+      }
+    }
   },
   autoControl(commentCount) {
     if (commentCount > 0 && window.innerWidth > 1500) {
@@ -30,13 +42,13 @@ module.exports = React.createClass({
       return '' !== b.comment
     }).map((b) => {
       const e = (
-        <Comment key={b.user} bookmark={b} store={this.props.store} />
+        <Comment key={b.user} bookmark={b} store={this.props.store} pos={this.state.pos} />
       )
       comments.push(e)
     })
     this.autoControl(comments.length)
     return (
-      <div className="ceg__comments">
+      <div className="ceg__comments" ref="TheCommentList" data-pos={this.state.pos}>
         {comments}
       </div>
     )
